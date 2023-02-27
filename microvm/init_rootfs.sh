69798bc74300bbcb81c4a8636da7eff3a637ec4a
Nathan Jones <nathanjones.ec@gmail.com>
2023-02-27 17:51:26 +0000

    Remove unnecessary features of VM

--- microvm/init_rootfs.sh
+++ microvm/init_rootfs.sh
@@ -1,24 +1,11 @@
 #!/bin/sh 
 
 apk add openrc
-apk add util-linux
-apk add curl
-
-# Set up a terminal on the serial console (ttyS0):
-echo "agetty_options=\"--autologin root --noclear\"" > /etc/conf.d/agetty-autologin
-ln -s agetty /etc/init.d/agetty-autologin.ttyS0
-echo ttyS0 > /etc/securetty
-rc-update add agetty-autologin.ttyS0 default
-
-# Make sure special file systems are mounted on boot:
-rc-update add devfs boot
-rc-update add procfs boot
-rc-update add sysfs boot
 
 rc-update add local default
 
-# Then, copy the newly configured system to the rootfs image:
-for d in bin etc lib root sbin usr; do tar c "/$d" | tar x -C /my-rootfs; done
+# Copy the newly configured system to the rootfs image:
+for d in bin etc lib sbin usr; do tar c "/$d" | tar x -C /my-rootfs; done
 
 # The above command may trigger the following message:
 # tar: Removing leading "/" from member names

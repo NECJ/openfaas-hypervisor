6792285405b50d4344f017dfb48325502250cce0
Nathan Jones <nathanjones.ec@gmail.com>
2023-03-20 16:17:26 +0000

    Update test file

--- test_simultaneous_invoke.sh
+++ test_simultaneous_invoke.sh
@@ -3,6 +3,14 @@
 for i in {1..2}
 do
    curl -X POST 'localhost:8080/function/calc-pi' &
+   curl -X POST 'localhost:8080/function/hello-world' &
+   curl -X POST 'localhost:8080/function/calc-pi' &
+   curl -X POST 'localhost:8080/function/hello-world' &
+   curl -X POST 'localhost:8080/function/calc-pi' &
+   curl -X POST 'localhost:8080/function/hello-world' &
+   curl -X POST 'localhost:8080/function/calc-pi' &
+   curl -X POST 'localhost:8080/function/hello-world' &
+   curl -X POST 'localhost:8080/function/calc-pi' &
 done
 
 wait

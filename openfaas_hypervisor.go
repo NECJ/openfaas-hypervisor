1ff8e818c7379460894baeafac1e54f0ddeabbda
Nathan Jones <nathanjones.ec@gmail.com>
2023-03-24 14:00:37 +0000

    Increase the amount of memory per VM

--- openfaas_hypervisor.go
+++ openfaas_hypervisor.go
@@ -257,7 +257,7 @@ func provisionFunctionInstance(functionName string) InstanceMetadata {
 	metadata.ip = ipIterator.Next()
 	macAddr := RandomMacAddress()
 	kernelPath := fmt.Sprintf(kernelPathTemplate, functionName)
-	qemuCmd := exec.Command(`qemu-system-x86_64`, `-netdev`, `tap,id=en0,ifname=`+tapName+`,script=no,downscript=no`, `-device`, `virtio-net-pci,netdev=en0,mac=`+macAddr, `-kernel`, kernelPath, `-append`, `netdev.ipv4_addr=`+metadata.ip+` netdev.ipv4_gw_addr=`+bridgeIp+` netdev.ipv4_subnet_mask=255.255.255.0 -- `+bridgeIp, `-cpu`, `host`, `-smp`, `1`, `-enable-kvm`, `-nographic`, `-m`, `5M`)
+	qemuCmd := exec.Command(`qemu-system-x86_64`, `-netdev`, `tap,id=en0,ifname=`+tapName+`,script=no,downscript=no`, `-device`, `virtio-net-pci,netdev=en0,mac=`+macAddr, `-kernel`, kernelPath, `-append`, `netdev.ipv4_addr=`+metadata.ip+` netdev.ipv4_gw_addr=`+bridgeIp+` netdev.ipv4_subnet_mask=255.255.255.0 -- `+bridgeIp, `-cpu`, `host`, `-smp`, `1`, `-enable-kvm`, `-nographic`, `-m`, `10M`)
 	metadata.vmStartTime = time.Now()
 
 	err = qemuCmd.Start()

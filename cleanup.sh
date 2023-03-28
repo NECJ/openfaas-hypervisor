f97405627f18124abc98c10dc6715cd958de4be3
Nathan Jones <nathanjones.ec@gmail.com>
2023-03-28 11:31:26 +0000

    Update tests to spawn more vms

--- cleanup.sh
+++ cleanup.sh
@@ -4,7 +4,7 @@ sudo kill $(pgrep qemu) &>/dev/null
 
 sudo ip link set dev ofhbr down &>/dev/null
 sudo brctl delbr ofhbr &>/dev/null
-for i in $(seq 1 100)
+for i in $(seq 1 500)
 do
     sudo ip link set dev ofhtap$i down &>/dev/null
     sudo ip tuntap del dev ofhtap$i mode tap &>/dev/null

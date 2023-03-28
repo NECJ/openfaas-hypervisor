5db8a8a146bb3bd77e36b44f1a62774d737fe2ab
Nathan Jones <nathanjones.ec@gmail.com>
2023-03-28 12:31:28 +0000

    update cleanup for firecracker

--- cleanup.sh
+++ cleanup.sh
@@ -2,10 +2,10 @@ sudo kill $(pgrep openfaas_hyperv) &>/dev/null
 
 sudo kill $(pgrep qemu) &>/dev/null
 
-sudo ip link set dev ofhbr down &>/dev/null
-sudo brctl delbr ofhbr &>/dev/null
-for i in $(seq 1 500)
-do
-    sudo ip link set dev ofhtap$i down &>/dev/null
-    sudo ip tuntap del dev ofhtap$i mode tap &>/dev/null
+for veth in $(ip addr | grep "veth" | cut -d' ' -f2 | sed 's/://' | sed 's/@if2//'); 
+do 
+    sudo ifconfig $veth down
+    sudo ip link del $veth
 done
+
+sudo rm -r /tmp/openfaas-hypervisor-vm*

6e9089caeed0ac7e8779086647359e3fc10b2a85
Nathan Jones <nathanjones.ec@gmail.com>
2023-03-29 21:42:40 +0000

    Update test script to run more tests

--- tests.sh
+++ tests.sh
@@ -5,42 +5,57 @@ cold_start()
    # Create data file
    datafile="cold_start_data.csv"
    echo "NumbInitVms,VmInitTimeNanoAvg,VmInitTimeNanoStd,VmInitTimeNano95,VmInitTimeNanoMax,FuncExecTimeNanoAvg,FuncExecTimeNanoStd,FuncExecTimeNano95,FuncExecTimeNanoMax" >> $datafile
-   for invokes in $(seq 1 1 1000)
+   for invokes in $(seq 1 1 60)
    do
-      echo "Number: $invokes"
-      # Start server
-      DISABLE_VM_REUSE=TRUE ./openfaas_hypervisor &
-      openfaas_pid=$!
-      trap "kill -SIGINT $openfaas_pid" EXIT
-
-      # Wait for server to start up
-      sleep 4
-
-      # Invoke function
-      curlPids=()
-      for j in $(seq 1 $invokes)
-      do
-         curl -X POST 'localhost:8080/function/calc-pi' &>/dev/null &
-         curlPids+=($!)
-      done
-      # Wait for all curl processes to finish
-      for pid in ${curlPids[@]}; do
-         wait $pid
-      done
-
-      sleep 4
-
-      # Get stats
-      curl -s -X POST 'localhost:8080/stats' | jq -r '[.NumbInitVms, .VmInitTimeNanoAvg, .VmInitTimeNanoStd, .VmInitTimeNano95, .VmInitTimeNanoMax, .FuncExecTimeNanoAvg, .FuncExecTimeNanoStd, .FuncExecTimeNano95, .FuncExecTimeNanoMax] | @csv' >> $datafile
-
-      # End server
-      echo "Shutting down server..."
-      if ! kill -SIGINT $openfaas_pid ; then
-         exit
-      fi
-      wait $openfaas_pid
-      sleep 4
+      cold_start_internal $invokes
    done
+
+   for invokes in $(seq 60 10 100)
+   do
+      cold_start_internal $invokes
+   done
+
+   for invokes in $(seq 100 100 1000)
+   do
+      cold_start_internal $invokes
+   done
+}
+
+cold_start_internal()
+{
+   echo "Number: $1"
+   # Start server
+   DISABLE_VM_REUSE=TRUE ./openfaas_hypervisor &
+   openfaas_pid=$!
+   trap "kill -SIGINT $openfaas_pid" EXIT
+
+   # Wait for server to start up
+   sleep 4
+
+   # Invoke function
+   curlPids=()
+   for j in $(seq 1 $1)
+   do
+      curl -X POST 'localhost:8080/function/calc-pi' &>/dev/null &
+      curlPids+=($!)
+   done
+   # Wait for all curl processes to finish
+   for pid in ${curlPids[@]}; do
+      wait $pid
+   done
+
+   sleep 4
+
+   # Get stats
+   curl -s -X POST 'localhost:8080/stats' | jq -r '[.NumbInitVms, .VmInitTimeNanoAvg, .VmInitTimeNanoStd, .VmInitTimeNano95, .VmInitTimeNanoMax, .FuncExecTimeNanoAvg, .FuncExecTimeNanoStd, .FuncExecTimeNano95, .FuncExecTimeNanoMax] | @csv' >> $datafile
+
+   # End server
+   echo "Shutting down server..."
+   if ! kill -SIGINT $openfaas_pid ; then
+      exit
+   fi
+   wait $openfaas_pid
+   sleep 4
 }
 
 warm_start()
@@ -48,52 +63,67 @@ warm_start()
    # Create data file
    datafile="warm_start_data.csv"
    echo "NumbInitVms,FuncExecTimeNanoAvg,FuncExecTimeNanoStd,FuncExecTimeNano95,FuncExecTimeNanoMax" >> $datafile
-   for invokes in $(seq 1 1 1000)
+   for invokes in $(seq 1 1 60)
    do
-      echo "Number: $invokes"
-      # Start server
-      ./openfaas_hypervisor &
-      openfaas_pid=$!
-      trap "kill -SIGINT $openfaas_pid" EXIT
-
-      # Wait for server to start up
-      sleep 4
-
-      # Pre-boot VMs
-      curl -X POST --data-raw $invokes 'localhost:8080/preBoot/calc-pi'
-      # Wait for them to boot
-      booted=$(curl -s -X POST 'localhost:8080/stats' | jq -r '.NumbInitVms')
-      while [[ $booted != $invokes ]]
-      do
-         sleep 0.5
-         booted=$(curl -s -X POST 'localhost:8080/stats' | jq -r '.NumbInitVms')
-      done
-
-      # Invoke function
-      curlPids=()
-      for j in $(seq 1 $invokes)
-      do
-         curl -X POST 'localhost:8080/function/calc-pi' &>/dev/null &
-         curlPids+=($!)
-      done
-      # Wait for all curl processes to finish
-      for pid in ${curlPids[@]}; do
-         wait $pid
-      done
-
-      sleep 4
-
-      # Get stats
-      curl -s -X POST 'localhost:8080/stats' | jq -r '[.NumbInitVms, .FuncExecTimeNanoAvg, .FuncExecTimeNanoStd, .FuncExecTimeNano95, .FuncExecTimeNanoMax] | @csv' >> $datafile
-
-      # End server
-      echo "Shutting down server..."
-      if ! kill -SIGINT $openfaas_pid ; then	
-         exit	
-      fi
-      wait $openfaas_pid
-      sleep 4
+      warm_start_internal $invokes
    done
+
+   for invokes in $(seq 60 10 100)
+   do
+      warm_start_internal $invokes
+   done
+
+   for invokes in $(seq 100 100 1000)
+   do
+      warm_start_internal $invokes
+   done
+}
+
+warm_start_internal() 
+{
+   echo "Number: $1"
+   # Start server
+   ./openfaas_hypervisor &
+   openfaas_pid=$!
+   trap "kill -SIGINT $openfaas_pid" EXIT
+
+   # Wait for server to start up
+   sleep 4
+
+   # Pre-boot VMs
+   curl -X POST --data-raw $1 'localhost:8080/preBoot/calc-pi'
+   # Wait for them to boot
+   booted=$(curl -s -X POST 'localhost:8080/stats' | jq -r '.NumbInitVms')
+   while [[ $booted != $1 ]]
+   do
+      sleep 0.5
+      booted=$(curl -s -X POST 'localhost:8080/stats' | jq -r '.NumbInitVms')
+   done
+
+   # Invoke function
+   curlPids=()
+   for j in $(seq 1 $1)
+   do
+      curl -X POST 'localhost:8080/function/calc-pi' &>/dev/null &
+      curlPids+=($!)
+   done
+   # Wait for all curl processes to finish
+   for pid in ${curlPids[@]}; do
+      wait $pid
+   done
+
+   sleep 4
+
+   # Get stats
+   curl -s -X POST 'localhost:8080/stats' | jq -r '[.NumbInitVms, .FuncExecTimeNanoAvg, .FuncExecTimeNanoStd, .FuncExecTimeNano95, .FuncExecTimeNanoMax] | @csv' >> $datafile
+
+   # End server
+   echo "Shutting down server..."
+   if ! kill -SIGINT $openfaas_pid ; then	
+      exit	
+   fi
+   wait $openfaas_pid
+   sleep 4
 }
 
 help()

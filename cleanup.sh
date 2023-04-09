12140983c399870b57833c9ae08f7bf004e199c8
Nathan Jones <nathanjones.ec@gmail.com>
2023-04-09 13:11:04 +0000

    Remove containers when cleaning up

--- cleanup.sh
+++ cleanup.sh
@@ -2,6 +2,10 @@ sudo kill $(pgrep openfaas_hyperv) &>/dev/null
 
 sudo kill $(pgrep firecracker) &>/dev/null
 sudo kill $(pgrep qemu) &>/dev/null
+for id in $(sudo runsc list | cut -d ' ' -f1); 
+do 
+    sudo runsc kill $id
+done
 
 sudo ip link set dev ofhbr down &>/dev/null
 sudo brctl delbr ofhbr &>/dev/null
